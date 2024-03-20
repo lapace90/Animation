e0d3361e2c8ddf477fd0ea359455a71ad24db602
Ilaria <lapace90@gmail.com>
2024-03-20 16:58:14 +0100

    clean

--- app/src/main/java/com/example/animation/Rotate.java
+++ app/src/main/java/com/example/animation/Rotate.java
@@ -5,7 +5,7 @@ import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
-public class Rotate extends MainActivity {
+public class Rotate {
     public static void startRotationAnimation(ImageView imageView, float fromDegrees, float toDegrees, long duration) {
         RotateAnimation rotateAnimation = new RotateAnimation(fromDegrees, toDegrees,
                 Animation.RELATIVE_TO_SELF, 0.5f,
